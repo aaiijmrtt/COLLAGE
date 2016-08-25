4dac46a7b8492e264f6b99e0ec804406cd482693
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2016-08-26 01:51:53 +0530

    basic framework

---
+++ README.md
@@ -0,0 +1,35 @@
+# COLLAGE
+
+Stitch Little Images Into Larger Images
+
+**Instructions**:
+
+* **Setting Executable Bit**: eg. `$chmod +x collage.py`
+* **Browsing Usage**: eg. `./collage.py -h`
+* **Convert Output Images to Video**: eg `ffmpeg -r 25 -i output/frame-%05d.jpg -c:v libx264 -vf fps=25 -pix_fmt yuv420p out.mp4`
+
+**Examples**:
+
+Here are some examples:
+
+![1 x 1](lena1.jpg)
+
+Lena
+
+![5 x 5](lena5.jpg)
+
+Some More Lena
+
+![25 x 25](lena25.jpg)
+
+Even More Lena
+
+![100 x 100](lena100.jpg)
+
+A Lot of Lena
+
+**Notes**:
+
+* Requires python.
+* Requires pyopencv.
+* Requires ffmpeg.
---
+++ collage.py
@@ -0,0 +1,52 @@
+import sys, getopt
+import cv2
+import util
+
+def image(sources, destination, step, output):
+	cv2.imwrite('%s.jpg' %output, util.collage(sources, destination, step))
+
+def video(sources, destination, step, output):
+	count, total = 0, destination.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+	while True:
+		if not destination.grab(): break
+		_source, _destination = list(), destination.retrieve()[1]
+		for i in xrange(len(sources)):
+			if not sources[i].grab(): break
+			_source.append(sources[i].retrieve()[1])
+		else:
+			cv2.imwrite('%s-%05d.jpg' %(output, count), util.collage(_source, _destination, step))
+			if count % 10 == 0:
+				sys.stdout.write('\r[%s>%s] %d%%' % ('=' * int(count / total * 20), ' ' * int((1 - count / total) * 20), int(count / total * 100)))
+				sys.stdout.flush()
+			count += 1
+			continue
+		break
+
+def arguments(string):
+	try:
+		opts, args = getopt.getopt(string, 'hvs:t:o:', ['help', 'video', 'sources=', 'times=', 'output='])
+		if len(args) != 1: raise Exception()
+		sources, destination, step, isvideo, output = [args[0]], args[0], 50, False, 'output'
+		for opt, arg in opts:
+			if opt in ('-h', '--help'): raise Exception()
+			elif opt in ('-v', '--video'): isvideo = True
+			elif opt in ('-s', '--sources'): sources = [line.strip() for line in open(arg)]
+			elif opt in ('-t', '--times'): step = int(arg)
+			elif opt in ('-o', '--output'): output = arg
+	except Exception as error:
+		print >> sys.stderr, util.usage
+		sys.exit()
+	return sources, destination, step, isvideo, output
+
+if __name__ == '__main__':
+	sources, destination, step, isvideo, output = arguments(sys.argv[1: ])
+	if not isvideo:
+		sources = [cv2.imread(source) for source in sources]
+		destination = cv2.imread(destination)
+		image(sources, destination, step, output)
+	else:
+		sources = [cv2.VideoCapture(source) for source in sources]
+		destination = cv2.VideoCapture(destination)
+		video(sources, destination, step, output)
+	 	for i in xrange(len(sources)): sources[i].release()
+		destination.release()
